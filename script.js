05669d38dfc229bd5b73dc59794a6f1f7397c35a
carlos19940319 <carlosmira2018@gmail.com>
2025-09-24 19:12:11 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ document.addEventListener('DOMContentLoaded', ()=>{
   const links = document.querySelectorAll('nav a');
   links.forEach(l=>{ if(location.pathname.endsWith(l.getAttribute('href')) || (location.pathname.endsWith('/') && l.getAttribute('href')==='index.html')) l.classList.add('active'); });
 });
+.ev-inner {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start; /* 👈 en lugar de center */
+  align-items: center;
+  padding: 24px;
+  box-sizing: border-box;
+  padding-top: 8vh; /* 👈 esto empuja el carrusel un poco desde arriba */
+}
