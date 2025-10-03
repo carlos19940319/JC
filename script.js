4609e1faad7346df82544f7bd0aaaf249d33a464
carlos19940319 <carlosmira2018@gmail.com>
2025-10-03 12:05:02 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const links = document.querySelectorAll('nav a');
-  const main = document.querySelector('main'); // ⚡ Apuntamos al main
 
-  // Pestaña activa
-  links.forEach(l => { 
-    if (
-      location.pathname.endsWith(l.getAttribute('href')) || 
-      (location.pathname.endsWith('/') && l.getAttribute('href') === 'index.html')
-    ) {
-      l.classList.add('active'); 
-    }
-  });
-
-  // Animación de entrada
-  requestAnimationFrame(() => {
-    main.classList.add('mostrar');
-  });
-
-  // Animación de salida al cambiar de pestaña
-  links.forEach(link => {
-    link.addEventListener('click', e => {
-      e.preventDefault();
-      const destino = link.getAttribute('href');
-      main.classList.remove('mostrar'); // inicia animación de salida
-      setTimeout(() => {
-        window.location.href = destino;
-      }, 400); // coincide con transición CSS
-    });
-  });
+document.addEventListener('DOMContentLoaded', ()=>{
+const links = document.querySelectorAll('nav a');
+links.forEach(l=>{ if(location.pathname.endsWith(l.getAttribute('href')) || (location.pathname.endsWith('/') && l.getAttribute('href')==='index.html')) l.classList.add('active'); });
 });
+
