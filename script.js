94e8244b594866a2c312207a8b08d4142f198086
carlos19940319 <carlosmira2018@gmail.com>
2025-12-14 14:26:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -202,4 +202,4 @@ document.addEventListener('DOMContentLoaded', () => {
   const yearEl = document.getElementById('year');
   if (yearEl) yearEl.textContent = new Date().getFullYear();
 
-}); 
+});
